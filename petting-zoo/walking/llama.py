10ff30be0a5343c8f5b95bb381db4041b9227a0d
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-06 16:40:46 -0600

    added shift property to animal classes with walking property. Added animal instances back to index and tested by printing donkey

--- petting-zoo/walking/llama.py
+++ petting-zoo/walking/llama.py
@@ -3,6 +3,7 @@ from walking.petting_area import PettingArea
 
 class Llama(PettingArea):
 
-   def __init__(self, name, species):
+   def __init__(self, name, species, shift):
     super().__init__(name, species)
     self.area = "Petting Area"
+    self.shift = shift
